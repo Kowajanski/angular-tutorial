0de68e43bc1b7534a0331e8d8926114b1c05a36f
Kowajanski <jnkwlwsk@gmail.com>
2019-10-12 22:42:56 +0200

    Added notify button

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-
+  /*
+  The @Input() decorator indicates that the property value passes in from the component's parent, the product list component
+  */
+  @Input() product;
   constructor() { }
 
   ngOnInit() {
